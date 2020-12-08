8c6a360e7ea8d839e35a6712e4c4744be730dff1
muddybootscode <firsttogo98@gmail.com>
2020-12-08 10:45:33 -0400

    Feature: Working with generics

--- contacts/views.py
+++ contacts/views.py
@@ -1,27 +1,33 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import mixins
 from rest_framework import generics
 from contacts.serializers import ContactSerializer
 from contacts.models import Contact
 
 
-# class ContactView(generics.GenericAPIView):
-#     serializer_class = ContactSerializer
-#     queryset = Contact.objects.all()
-
-class ContactView(APIView):
-    """
-    Return Contacts
-    """
+class ContactView(mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
+    lookup_field = 'slug'
 
     def get(self, request, *args, **kwargs):
-        qs = Contact.objects.all()
-        serializer = ContactSerializer(qs, many=True)
-        return Response(serializer.data)
+        return self.list(self, request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        serializer = ContactSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+
+# class ContactView(APIView):
+#     """
+#     Return Contacts
+#     """
+#
+    # def get(self, request, *args, **kwargs):
+    #     qs = Contact.objects.all()
+    #     serializer = ContactSerializer(qs, many=True)
+    #     return Response(serializer.data)
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = ContactSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
