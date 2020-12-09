a64afba0a92aba144faddced7af509b41e9a5289
muddybootscode <firsttogo98@gmail.com>
2020-12-09 08:37:24 -0400

    Refactor: Changed url structure

--- django_address_api/urls.py
+++ django_address_api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('contacts/', ContactListView.as_view(), name='test'),
     path('create_contact/', ContactCreateView.as_view(), name='create'),
-    path('contacts/<slug:slug>', ContactSlugView.as_view(), name='slug_view'),
+    path('contact/<slug:slug>', ContactSlugView.as_view(), name='slug_view'),
     path('edit_contact/<slug:slug>', ContactEditView.as_view(), name='edit_view'),
     path('delete_contact/<slug:slug>', ContactDeleteView.as_view(), name='edit_view'),
     path('api-auth/', include('rest_framework.urls'))
